f5fef3ee8706c297b8bee8a53d1336165409ca2d
rezerbit <rezerbit@gmail.com>
2015-05-03 16:12:50 +0500

    prepare the app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  get '/secret', to: 'pages#index', as: :secret
+  root to: 'pages#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
