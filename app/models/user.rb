aa1e2fb10914140629861490add163006e9be5ec
rezerbit <rezerbit@gmail.com>
2015-05-03 16:28:05 +0500

    integrate sorcery

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ActiveRecord::Base
+  authenticates_with_sorcery!
+
+  validates :password, length: { minimum: 3 }
+  validates :password, confirmation: true
+  validates :email, uniqueness: true, email_format: { message: 'has invalid format' }
+end
